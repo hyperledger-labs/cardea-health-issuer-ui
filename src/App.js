91b19e868a842cf23301e863e88be793b8c4f78c
Mike Ebert <michael.k.ebert@gmail.com>
2022-06-07 18:04:00 -0600

    fix: repair merging problems
    Signed-off-by: Mike Ebert <michael.k.ebert@gmail.com>

--- src/App.js
+++ src/App.js
@@ -42,17 +42,16 @@ import SessionProvider from './UI/SessionProvider'
 import './App.css'
 
 const Frame = styled.div`
-  /*display: flex;
+  display: flex;
   flex-direction: row;
-  flex-wrap: nowrap;*/
-  display: grid;
-  grid-template-columns: 1fr min-content;
-  grid-template-columns: 240px 1fr;
+  flex-wrap: nowrap;
+  /*display: grid;
+  grid-template-columns: 240px 1fr;*/
 `
 const Main = styled.main`
-  /*flex: 9;*/
-  grid-row: 1;
-  grid-column: 2;
+  flex: 9;
+  /*grid-row: 1;
+  grid-column: 2;*/
   padding: 30px;
 `
 
