c59fadbac8e014db3316b0631817d1109a7388f8
RomanStepanyan <stepik10@gmail.com>
2022-12-13 19:16:45 +0100

    chore: changing the border width
    Signed-off-by: RomanStepanyan <stepik10@gmail.com>

--- src/App.js
+++ src/App.js
@@ -65,7 +65,7 @@ function App() {
     positive_color: '#008a00',
     text_color: '#555',
     text_light: '#fff',
-    border: '3px solid #e3e3e3',
+    border: '1px solid #e3e3e3',
     drop_shadow: '3px 3px 3px rgba(0, 0, 0, 0.3)',
     background_primary: '#fff',
     background_secondary: '#f5f5f5',
